3b6a3df8ffeaedf77c4b74bf03200c02df854175
Uddhav-singh <uddhav.singh.79@gmail.com>
2025-09-14 21:58:03 +0530

    adding the cart feature.

---
+++ backend/controllers/shopController.js
@@ -0,0 +1,11 @@
+// userSchema.methods.addToCart = function() {}
+
+// userSchema = name of the user model schema
+
+// methods = methods key here is an object that allows you to add your own method(eg. addToCart )
+
+// addToCart = addToCart  is the method name which we have given to the method.
+
+
+
+// function (){} = it has to be function like that only so that "this" keyword inside can refer to the userSchema Schema not something else.
